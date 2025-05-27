6fdc66b0b6a6acae712dd9de95f42683917c6f1d
cattanich <cattanich@gmail.com>
2025-05-27 11:44:25 -0500

    Agregar parametros al admin-firebase para correcta auth

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Inicializar Firebase Admin SDK
-admin.initializeApp();
+if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
+  throw new Error('Missing Firebase configuration in environment variables.');
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+  }),
+});
 
 const db = admin.firestore();
 
